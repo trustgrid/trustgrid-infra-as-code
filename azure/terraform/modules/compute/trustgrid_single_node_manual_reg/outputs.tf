36243482a1703986e192ba425439dc7466e9e979
Marquis Calmes <marquis@trustgrid.io>
2025-01-16 10:23:14 -0600

    organize and rename outputs

--- azure/terraform/modules/compute/trustgrid_single_node_manual_reg/outputs.tf
+++ azure/terraform/modules/compute/trustgrid_single_node_manual_reg/outputs.tf
@@ -1,3 +1,4 @@
+## Virtual Machine outputs
 
 output "vm_id" {
   description = "ID of the created virtual machine"
@@ -9,14 +10,26 @@ output "vm_name" {
   value       = azurerm_linux_virtual_machine.node.name
 }
 
-output "public_ip_address" {
+output "principal_id" {
+  description = "Principal ID of the system assigned identity"
+  value       = azurerm_linux_virtual_machine.node.identity[0].principal_id
+}
+
+
+## public interface outputs
+output "public_nic_public_ip_address" {
   description = "Public IP address assigned to the VM"
   value       = azurerm_public_ip.public_ip.ip_address
 }
 
-output "private_ip_address" {
-  description = "Private IP address of the primary interface"
-  value       = azurerm_network_interface.private.private_ip_address
+output "public_nic_private_ip_address" {
+  description = "Private IP address of the public interface"
+  value       = azurerm_network_interface.public.private_ip_address
+}
+
+output "public_nic_mac_address" {
+  description = "MAC address of the public interface"
+  value       = azurerm_network_interface.public.mac_address
 }
 
 output "public_nic_id" {
@@ -24,12 +37,14 @@ output "public_nic_id" {
   value       = azurerm_network_interface.public.id
 }
 
+
+## private interface outputs
+output "private_ip_address" {
+  description = "Private IP address of the primary interface"
+  value       = azurerm_network_interface.private.private_ip_address
+}
 output "private_nic_id" {
   description = "ID of the private network interface"
   value       = azurerm_network_interface.private.id
 }
 
-output "principal_id" {
-  description = "Principal ID of the system assigned identity"
-  value       = azurerm_linux_virtual_machine.node.identity[0].principal_id
-}
