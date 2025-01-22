f2c256dfc9c7f15de66144e7339582a3e50f86a6
Marquis Calmes <marquis@trustgrid.io>
2025-01-22 09:42:30 -0600

    update outputs

--- azure/terraform/modules/authorization/trustgrid_cluster_ip_role/outputs.tf
+++ azure/terraform/modules/authorization/trustgrid_cluster_ip_role/outputs.tf
@@ -1,4 +1,10 @@
-output "role_definition_id" {
+output "id" {
   value = azurerm_role_definition.tg_cluster_ip_role.role_definition_id
   description = "The Azure ID for the Trustgrid Cluster IP role"
+}
+
+output "name" {
+  value = azurerm_role_definition.tg_cluster_ip_role.name
+  description = "The name of the Trustgrid Cluster IP role"
+  
 }
