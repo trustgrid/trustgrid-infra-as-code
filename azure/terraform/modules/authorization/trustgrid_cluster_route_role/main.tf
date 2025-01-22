bb263e195e6a42ceaaed6fea5d30a5f9ff0d960a
Marquis Calmes <marquis@trustgrid.io>
2025-01-22 10:55:29 -0600

    cluster route table first try

---
+++ azure/terraform/modules/authorization/trustgrid_cluster_route_role/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.15.0"
+    }
+  }
+}
+
+resource "azurerm_role_definition" "tg_cluster_ip_role" {
+  name  = "${var.name_prefix}-trustgrid-cluster-ip-role"
+  scope = var.scope
+  description = "Provides permissions required for Trustgrid clusters to manage updating routes in Azure"
+  permissions {
+    actions = [
+      "Microsoft.Network/networkWatchers/nextHop/action",
+      "Microsoft.Network/networkInterfaces/effectiveRouteTable/action",
+      "Microsoft.Network/routeTables/routes/delete",
+      "Microsoft.Network/routeTables/routes/write",
+      "Microsoft.Network/routeTables/routes/read",
+      "Microsoft.Network/routeTables/join/action",
+      "Microsoft.Network/routeTables/delete",
+      "Microsoft.Network/routeTables/write",
+      "Microsoft.Network/routeTables/read",
+      "Microsoft.Network/networkInterfaces/read",
+      "Microsoft.Network/virtualNetworks/read",
+      "Microsoft.Compute/virtualMachines/read"
+    ]
+    not_actions = []
+    data_actions = []
+    not_data_actions = []
+  }
+  assignable_scopes = var.assignable_scopes
+}
