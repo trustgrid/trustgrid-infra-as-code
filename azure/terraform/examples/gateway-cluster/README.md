b00155dd9514770c8900fa4c20ae2fe1bf737d63
Marquis Calmes <marquis@trustgrid.io>
2025-01-24 10:27:32 -0600

    gateway example

---
+++ azure/terraform/examples/gateway-cluster/README.md
@@ -0,0 +1,85 @@
+### Terraform Azure Gateway Cluster Example
+This example demonstrates how to deploy a gateway cluster using Terraform. 
+
+The example use both the manual and automated registration modules to show how they work. In a real world scenario, you would use only one method to register.  
+
+***NOTE:*** Typically only direct Trustgrid customers deploy their nodes as [Gateways](https://docs.trustgrid.io/docs/nodes/#gateway-nodes).  Most Trustgrid deployments act only as [Edge](https://docs.trustgrid.io/docs/nodes/#edge-nodes) nodes that do not need inbound security group rules. 
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_azurerm"></a> [azurerm](#requirement\_azurerm) | ~> 4.15.0 |
+| <a name="requirement_cloudinit"></a> [cloudinit](#requirement\_cloudinit) | ~> 2.3.5 |
+| <a name="requirement_tg"></a> [tg](#requirement\_tg) | ~> 1.10.3 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_azurerm"></a> [azurerm](#provider\_azurerm) | ~> 4.15.0 |
+| <a name="provider_tg"></a> [tg](#provider\_tg) | ~> 1.10.3 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_az_gw1"></a> [az\_gw1](#module\_az\_gw1) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/compute/trustgrid_single_node_manual_reg | v0.1.0 |
+| <a name="module_az_gw2"></a> [az\_gw2](#module\_az\_gw2) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/compute/trustgrid_single_node_auto_reg | v0.1.0 |
+| <a name="module_trustgrid_cluster_ip_role"></a> [trustgrid\_cluster\_ip\_role](#module\_trustgrid\_cluster\_ip\_role) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/authorization/trustgrid_cluster_ip_role | v0.1.0 |
+| <a name="module_trustgrid_cluster_route_role"></a> [trustgrid\_cluster\_route\_role](#module\_trustgrid\_cluster\_route\_role) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/authorization/trustgrid_cluster_route_role | v0.1.0 |
+| <a name="module_trustgrid_gateway_security_group_rules"></a> [trustgrid\_gateway\_security\_group\_rules](#module\_trustgrid\_gateway\_security\_group\_rules) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/network/trustgrid_gateway_security_group_rules | v0.1.0 |
+| <a name="module_trustgrid_outbound_cp_rules"></a> [trustgrid\_outbound\_cp\_rules](#module\_trustgrid\_outbound\_cp\_rules) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/network/trustgrid_outbound_control_plane_security_group_rules | v0.1.0 |
+| <a name="module_trustgrid_outbound_dp_rules"></a> [trustgrid\_outbound\_dp\_rules](#module\_trustgrid\_outbound\_dp\_rules) | github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/network/trustgrid_outbound_data_plane_security_group_rules | v0.1.0 |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [azurerm_network_interface_application_security_group_association.az_gw1](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_application_security_group_association) | resource |
+| [azurerm_network_interface_application_security_group_association.az_gw2](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_application_security_group_association) | resource |
+| [azurerm_network_security_group.private](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group) | resource |
+| [azurerm_network_security_group.public](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group) | resource |
+| [azurerm_resource_group.trustgrid](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group) | resource |
+| [azurerm_role_assignment.az_gw1_cluster_ip](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
+| [azurerm_role_assignment.az_gw1_cluster_route_network](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
+| [azurerm_role_assignment.az_gw2_cluster_ip](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
+| [azurerm_role_assignment.az_gw2_cluster_route_network](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
+| [azurerm_subnet.private](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet) | resource |
+| [azurerm_subnet.public](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet) | resource |
+| [azurerm_virtual_network.main](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network) | resource |
+| [tg_license.az_gw2](https://registry.terraform.io/providers/trustgrid/tg/latest/docs/resources/license) | resource |
+| [azurerm_subscription.current](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subscription) | data source |
+| [tg_node.az_gw2](https://registry.terraform.io/providers/trustgrid/tg/latest/docs/data-sources/node) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_admin_ssh_key_pub"></a> [admin\_ssh\_key\_pub](#input\_admin\_ssh\_key\_pub) | Public Key for SSH access to the virtual machines. External SSH is disabled during registration, but a key is required by Azure to create the VM and may be needed if there are issues registering. | `string` | n/a | yes |
+| <a name="input_az_gw1_name"></a> [az\_gw1\_name](#input\_az\_gw1\_name) | Name for the first gateway VM, environment prefix will be prepended | `string` | n/a | yes |
+| <a name="input_az_gw1_zone"></a> [az\_gw1\_zone](#input\_az\_gw1\_zone) | Availability zone for the first gateway VM | `number` | n/a | yes |
+| <a name="input_az_gw2_name"></a> [az\_gw2\_name](#input\_az\_gw2\_name) | Name for the second gateway VM, environment prefix will be prepended | `string` | n/a | yes |
+| <a name="input_az_gw2_zone"></a> [az\_gw2\_zone](#input\_az\_gw2\_zone) | Availability zone for the second gateway VM | `number` | n/a | yes |
+| <a name="input_az_gw_host_name"></a> [az\_gw\_host\_name](#input\_az\_gw\_host\_name) | Name for the gateway host VM, environment prefix will be prepended | `string` | n/a | yes |
+| <a name="input_az_gw_host_zone"></a> [az\_gw\_host\_zone](#input\_az\_gw\_host\_zone) | Availability zone for the gateway host VM | `number` | n/a | yes |
+| <a name="input_environment_prefix"></a> [environment\_prefix](#input\_environment\_prefix) | The prefix used for all resources in this environment | `string` | n/a | yes |
+| <a name="input_location"></a> [location](#input\_location) | The location/region where the resources will be created | `string` | n/a | yes |
+| <a name="input_private_cidr"></a> [private\_cidr](#input\_private\_cidr) | The CIDR block for the private subnet | `string` | n/a | yes |
+| <a name="input_public_cidr"></a> [public\_cidr](#input\_public\_cidr) | The CIDR block for the public subnet | `string` | n/a | yes |
+| <a name="input_subscription_id"></a> [subscription\_id](#input\_subscription\_id) | The subscription ID where the resources will be created | `string` | n/a | yes |
+| <a name="input_tg_api_host"></a> [tg\_api\_host](#input\_tg\_api\_host) | The TrustGrid API host. Defaults to api.trustgrid.io and should not need to be changed. | `string` | `"api.trustgrid.io"` | no |
+| <a name="input_vnet_cidr"></a> [vnet\_cidr](#input\_vnet\_cidr) | The CIDR block for the virtual network | `string` | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_az_gw1_mac_address"></a> [az\_gw1\_mac\_address](#output\_az\_gw1\_mac\_address) | az\_gw1 mac address |
+| <a name="output_az_gw1_name"></a> [az\_gw1\_name](#output\_az\_gw1\_name) | az\_gw1 vm name |
+| <a name="output_az_gw1_public_ip"></a> [az\_gw1\_public\_ip](#output\_az\_gw1\_public\_ip) | az\_gw1 public ip |
+| <a name="output_az_gw2_mac_address"></a> [az\_gw2\_mac\_address](#output\_az\_gw2\_mac\_address) | az\_gw2 mac address |
+| <a name="output_az_gw2_name"></a> [az\_gw2\_name](#output\_az\_gw2\_name) | az\_gw2 vm name |
+| <a name="output_az_gw2_public_ip"></a> [az\_gw2\_public\_ip](#output\_az\_gw2\_public\_ip) | az\_gw2 public ip |
+<!-- END_TF_DOCS -->
---
+++ azure/terraform/examples/gateway-cluster/main.tf
@@ -0,0 +1,222 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.15.0"
+      }
+      tg = { 
+        ## Remove if you are not using the Trustgrid provider to generate  the TrustGrid license
+        source = "trustgrid/tg"
+        version = "~> 1.10.3"
+      }
+      cloudinit = {
+        source = "hashicorp/cloudinit"
+        version = "~> 2.3.5"
+      }
+    }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+provider "tg" {
+  ## Remove if you are not using the Trustgrid provider to generate  the TrustGrid license
+  api_host = var.tg_api_host
+}
+
+provider "cloudinit" {  
+}
+
+## Example Resource Group and Virtual Network creation. If using 
+resource "azurerm_resource_group" "trustgrid" {
+  name     = "${var.environment_prefix}-trustgrid-rg"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.environment_prefix}-vnet"
+  location            = azurerm_resource_group.trustgrid.location
+  resource_group_name = azurerm_resource_group.trustgrid.name
+  address_space       = [var.vnet_cidr]
+
+  tags = {
+    Environment = var.environment_prefix
+  }
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "${var.environment_prefix}-public"
+  resource_group_name  = azurerm_resource_group.trustgrid.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.public_cidr]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "${var.environment_prefix}-private"
+  resource_group_name  = azurerm_resource_group.trustgrid.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.private_cidr]
+}
+
+resource "azurerm_network_security_group" "public" {
+  name                = "${var.environment_prefix}-public-nsg"
+  location            = azurerm_resource_group.trustgrid.location
+  resource_group_name = azurerm_resource_group.trustgrid.name
+}
+
+resource "azurerm_network_security_group" "private" {
+  name                = "${var.environment_prefix}-private-nsg"
+  location            = azurerm_resource_group.trustgrid.location
+  resource_group_name = azurerm_resource_group.trustgrid.name
+}
+
+## Configure Security Group Rules for Trustgrid Gateways. These rules are required for the Trustgrid Gateways to function. The module can also optionally create rules for the Gateways to act as Wireguard or ZTNA Application gateways.  
+
+module "trustgrid_gateway_security_group_rules" {
+  source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/network/trustgrid_gateway_security_group_rules?ref=v0.1.0"
+
+  name_prefix                          = var.environment_prefix
+  security_group_id                    = resource.azurerm_network_security_group.public.id
+  security_group_rule_priority_start   = 200
+  
+  trustgrid_data_plane_gateway_tcp     = true  ## True is the default value so this line could be omitted
+  trustgrid_data_plane_gateway_tcp_port = "8443" ## 8443 is the default value so this line could be omitted
+  trustgrid_data_plane_gateway_udp     = true  ## True is the default value so this line could be omitted
+  trustgrid_data_plane_gateway_udp_port = "8443" ## 8443 is the default value so this line could be omitted
+}
+
+## Create Trustgrid Gateways
+### The below example shows creating one gateway with the manual registration module and the other with the automatic registration module. A real deployment should use the same module for both gateways depending on the desired registration method.
+module "az_gw1" {
+    source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/compute/trustgrid_single_node_manual_reg?ref=v0.1.0"
+    resource_group_name = azurerm_resource_group.trustgrid.name
+    location = azurerm_resource_group.trustgrid.location
+
+    name = "${var.environment_prefix}-${var.az_gw1_name}"
+    availability_zone = var.az_gw1_zone
+    admin_ssh_key_pub = var.admin_ssh_key_pub
+    
+    public_subnet_id = azurerm_subnet.public.id
+    public_security_group_id = azurerm_network_security_group.public.id
+    private_subnet_id = azurerm_subnet.private.id
+    private_security_group_id = azurerm_network_security_group.private.id
+    
+
+}
+
+
+### The below example uses the Trustgrid provider to generate the license for the gateway. Then uses the auto registration module to create the VM and have it register with the Trustgrid control plane automatically.
+resource "tg_license" "az_gw2" {
+    name = "${var.environment_prefix}-${var.az_gw2_name}"
+}
+
+module "az_gw2" {
+    source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/compute/trustgrid_single_node_auto_reg?ref=v0.1.0"
+    resource_group_name = azurerm_resource_group.trustgrid.name
+    location = azurerm_resource_group.trustgrid.location
+
+    name = "${var.environment_prefix}-${var.az_gw2_name}"
+    availability_zone = var.az_gw2_zone
+    admin_ssh_key_pub = var.admin_ssh_key_pub
+    
+    public_subnet_id = azurerm_subnet.public.id
+    public_security_group_id = azurerm_network_security_group.public.id
+    private_subnet_id = azurerm_subnet.private.id
+    private_security_group_id = azurerm_network_security_group.private.id
+
+    tg_license = tg_license.az_gw2.license
+  
+}
+
+## Use TG Provider to confirm registration and get UID. This is not required but if you plan to use the TG provider to configure the gateway, you will need the UID.
+data "tg_node" "az_gw2" {
+    fqdn = tg_license.az_gw2.fqdn
+    timeout = 300 # Time in seconds to wait for registration. 
+    depends_on = [ module.az_gw2 ]
+}
+
+## Assign GWs to Application Security Group
+resource "azurerm_network_interface_application_security_group_association" "az_gw1" {
+  network_interface_id = module.az_gw1.public_nic_id
+  application_security_group_id = module.trustgrid_gateway_security_group_rules.application_security_group_id
+}
+
+resource "azurerm_network_interface_application_security_group_association" "az_gw2" {
+  network_interface_id = module.az_gw2.public_nic_id
+  application_security_group_id = module.trustgrid_gateway_security_group_rules.application_security_group_id
+}
+## Explicitly Allow Traffic to the Trustgrid Control Plane 
+## This would only be required if the security group does not allow all outbound traffic to the Internet
+
+module "trustgrid_outbound_cp_rules" {
+  source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/network/trustgrid_outbound_control_plane_security_group_rules?ref=v0.1.0"
+  name_prefix = var.environment_prefix
+  security_group_id = azurerm_network_security_group.public.id
+  security_group_rule_priority_start = 300
+} 
+
+module "trustgrid_outbound_dp_rules" {
+  source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/network/trustgrid_outbound_data_plane_security_group_rules?ref=v0.1.0"
+  name_prefix = var.environment_prefix
+  security_group_id = azurerm_network_security_group.public.id
+  security_group_rule_priority_start = 310
+  enable_udp=var.data_plane_enable_udp
+  data_plane_endpoints = var.data_plane_endpoints
+
+} 
+
+## Below shows how to create Azure IAM Roles for managing Cluster High Availability with both the floating Cluster IP method and by managing route tables.  Typically you'd only use one of these methods.
+
+## Create Roles Required for Cluster IP and Assign to the Trustgrid Gateways
+data "azurerm_subscription" "current" {
+}
+
+
+
+module "trustgrid_cluster_ip_role" {
+    source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/authorization/trustgrid_cluster_ip_role?ref=v0.1.0"
+    name_prefix = var.environment_prefix
+    scope = data.azurerm_subscription.current.id
+    assignable_scopes = [ data.azurerm_subscription.current.id ]
+  
+}
+
+resource "azurerm_role_assignment" "az_gw1_cluster_ip" {
+    scope = azurerm_resource_group.trustgrid.id
+    role_definition_name = module.trustgrid_cluster_ip_role.name
+    principal_id = module.az_gw1.principal_id
+}
+
+resource "azurerm_role_assignment" "az_gw2_cluster_ip" {
+    scope = azurerm_resource_group.trustgrid.id
+    role_definition_name = module.trustgrid_cluster_ip_role.name
+    principal_id = module.az_gw2.principal_id
+}
+
+## Create Roles Required for Cluster Routes and Assign to the Trustgrid Gateways
+
+
+module "trustgrid_cluster_route_role" {
+    source = "github.com/trustgrid/trustgrid-infra-as-code//azure/terraform/modules/authorization/trustgrid_cluster_route_role?ref=v0.1.0"
+    name_prefix = var.environment_prefix
+    scope = data.azurerm_subscription.current.id
+    assignable_scopes = [ data.azurerm_subscription.current.id ]  
+    ## If the cluster needs to update route tables in other subscriptions, then the assignable_scopes should be the list of subscriptions that the cluster can manage routes in. 
+    ## You would also need additional role assignments with scopes in those subscriptions that include the needed route tables.
+}
+
+### Assign the role to the Trustgrid Gateways
+resource "azurerm_role_assignment" "az_gw1_cluster_route_network" {
+    scope = azurerm_resource_group.trustgrid.id
+    role_definition_name = module.trustgrid_cluster_route_role.name
+    principal_id = module.az_gw1.principal_id
+}
+
+
+resource "azurerm_role_assignment" "az_gw2_cluster_route_network" {
+    scope = azurerm_resource_group.trustgrid.id
+    role_definition_name = module.trustgrid_cluster_route_role.name
+    principal_id = module.az_gw2.principal_id
+}
